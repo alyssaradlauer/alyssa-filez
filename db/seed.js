fba1b11c69198414bf208c14d328f80b19a9311d
Alyssa <alyssaradlauer@gmail.com>
2025-12-10 09:32:52 -0700

    database part of block 39 complete

--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,29 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  const SQL = `
+  INSERT INTO folders(name) VALUES('red');
+  INSERT INTO folders(name) VALUES('green');
+  INSERT INTO folders(name) VALUES('blue');
+
+
+  INSERT INTO files(name, size, folder_id) VALUES ('red-file-1', 100, 1);
+  INSERT INTO files(name, size, folder_id) VALUES ('red-file-2', 110, 1);
+  INSERT INTO files(name, size, folder_id) VALUES ('red-file-3', 115, 1);
+  INSERT INTO files(name, size, folder_id) VALUES ('red-file-4', 120, 1);
+  INSERT INTO files(name, size, folder_id) VALUES ('red-file-5', 117, 1);
+
+  INSERT INTO files(name, size, folder_id) VALUES ('green-file-1', 100, 2);
+  INSERT INTO files(name, size, folder_id) VALUES ('green-file-2', 110, 2);
+  INSERT INTO files(name, size, folder_id) VALUES ('green-file-3', 115, 2);
+  INSERT INTO files(name, size, folder_id) VALUES ('green-file-4', 120, 2);
+  INSERT INTO files(name, size, folder_id) VALUES ('green-file-5', 117, 2);
+
+  INSERT INTO files(name, size, folder_id) VALUES ('blue-file-1', 100, 3);
+  INSERT INTO files(name, size, folder_id) VALUES ('blue-file-2', 110, 3);
+  INSERT INTO files(name, size, folder_id) VALUES ('blue-file-3', 115, 3);
+  INSERT INTO files(name, size, folder_id) VALUES ('blue-file-4', 120, 3);
+  INSERT INTO files(name, size, folder_id) VALUES ('blue-file-5', 117, 3);
+  `;
+  await db.query(SQL);
 }
